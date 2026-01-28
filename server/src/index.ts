64e4ac22ec942236e2ef4e752794d56979f45db0
srikar-04 <srikar.code01@gmail.com>
2026-01-28 10:20:38 +0530

    installed auth packages, added secrets to env and env.sample and added auth logic to app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,17 @@
-const greeting: string = "Hello, world!";
+import dotenv from 'dotenv'
+dotenv.config()
+import express from "express";
+import { ExpressAuth } from "@auth/express";
+import GitHub from '@auth/express/providers/github';
+import Google from '@auth/express/providers/google';
 
-console.log(greeting);
+const app = express()
+
+app.set("trust proxy", true)
+app.use("/auth", ExpressAuth({
+    providers: [GitHub, Google]
+}))
+
+app.listen(3000, () => {
+    console.log('app is listening on port 3000')
+})
