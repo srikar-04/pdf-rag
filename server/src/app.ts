7cbf548c75e56cefb2013468d0c17138f6f1ecaa
srikar-04 <srikar.code01@gmail.com>
2026-01-28 15:27:26 +0530

    installed cors and added couple of middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import cors from 'cors'
 import { ExpressAuth } from "@auth/express";
 import GitHub from '@auth/express/providers/github';
 import Google from '@auth/express/providers/google';
 
 const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(urlencoded({extended: true}))
 
 app.set("trust proxy", true)
 app.use("/auth", ExpressAuth({
