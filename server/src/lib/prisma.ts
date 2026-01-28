0a19175e16cd95eaa5d7c0c1700164542c2a2874
srikar-04 <srikar.code01@gmail.com>
2026-01-28 09:22:32 +0530

    created prisma client in lib folder

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client.js'
+
+const connectionString = process.env.DATABASE_URL
+
+if(!connectionString) throw new Error("cannot find connection string")
+
+const adapter = new PrismaPg({connectionString})
+
+const prisma = new PrismaClient({adapter})
+
+export { prisma }
